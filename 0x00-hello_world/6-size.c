d787e289c59d14929bb679e0f5443da021b1d0cb
johnkatua <johnkatua99@gmail.com>
2021-02-03 15:11:32 +0000

    added a C program that prints sizes of different types depending on computer used to run the code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 */
 int main(void)
 {
-	printf("Size of an int: %ld byte(s)\n", sizeof(int));
-	printf("Size of char: %ld byte(s)\n", sizeof(char));
-	printf("Size of float: %ld byte(s)\n", sizeof(float));
-	printf("Size of long int: %ld byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
 	return (0);
 }
