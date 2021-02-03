19fc8043b7b2a8fc72f7ee46498f534eeb0d013f
johnkatua <johnkatua99@gmail.com>
2021-02-03 14:41:19 +0000

    added a C program that prints an output using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Prints "with proper grammar, but the outcome is a piece of art,"
+*
+* Return: 0 if no output in the printf
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
