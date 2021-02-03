f54e613ce424de80a0536f90ca45d30d5ad4be4e
johnkatua <johnkatua99@gmail.com>
2021-02-03 15:48:59 +0000

    added a C program that prints sizes of different types depending on computer used to run the code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /**
-* main - Prints various type sizes based on the computer used to compile code
-*
-* Returns: 0 incase it runs into some errors or no data provided
-*/
+ * main - This function prints the sizes of different types
+ *Return: This function returns 0
+ */
 int main(void)
 {
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of char: %lu byte(s)\n", sizeof(char));
-	printf("Size of float: %lu byte(s)\n", sizeof(float));
-	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
