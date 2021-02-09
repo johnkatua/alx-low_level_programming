56d83892f44ebbede45cbc83a6b086b6d53bcd04
johnkatua <johnkatua99@gmail.com>
2021-02-09 13:55:30 +0000

    added a test function to check if an integer is positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - test function that prints if number is or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
