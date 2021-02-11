adf1c76dd57cc312cffad666564bba73dfba58ca
johnkatua <johnkatua99@gmail.com>
2021-02-11 06:30:43 +0000

    added a C program that multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul - It takes to arguments
+ * @a: Is the first used in the manipulation
+ * @b: Is the second argument in the function
+ * It multiplies two arguments to get the output
+ * Return: The product of two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
