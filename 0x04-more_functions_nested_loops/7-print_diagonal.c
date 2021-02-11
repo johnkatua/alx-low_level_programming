11308059c12d63be1174e9effcefe72bb4ccb247
johnkatua <johnkatua99@gmail.com>
2021-02-11 10:06:35 +0000

    added a C program that draws a diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_diagonal - It takes one argument
+ * @n: length of line
+ * draws a diagonal line
+ *
+ * Returns: returns void
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
