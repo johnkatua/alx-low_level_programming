0fc3b61a8577d4f3df45f5aa5fdf93c76a754e23
johnkatua <johnkatua99@gmail.com>
2021-02-11 07:09:17 +0000

    added a C program that prints out number from 0 to 9 and exclude 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - It takes no arguments
+ * It prints numbers from 0 to 9 excluding 2 and 4
+ *
+ * Return: returns void
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num = '9'; num++)
+	{
+		if (num == '2' || num == '4')
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(num);
+		}
+	}
+	_putchar('\n');
+}
