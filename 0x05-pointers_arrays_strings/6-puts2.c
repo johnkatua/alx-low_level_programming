c3111f550e0581479f783f4753ecef525ac8f044
johnkatua <johnkatua99@gmail.com>
2021-02-15 18:43:07 +0000

    added a C program to print single character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include  "holberton.h"
+/**
+ * puts2 - It has only one parameter
+ * It prints character of a string
+ * @str: String whose characters are displayed
+ * Return: returns void
+ */
+void puts2(char *str)
+{
+	int idx  = 0, len = 0;
+
+	while (str[idx++])
+		len++;
+
+	for (idx = o; idx < len; idx += 1)
+	{
+		_putchar(s[idx];
+	}
+	_putchar('\n');
+}
