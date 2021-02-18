df5c2fe55fd78fc286d7b57fb386ac836230a8c0
johnkatua <johnkatua99@gmail.com>
2021-02-18 16:21:17 +0000

    updated my file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,11 @@ char *_strncat(char *dest, char *src, int n)
 	int a, b;
 
 	for  (a = 0; dest[a] != '\0'; a++)
-	{
+		;
 		for (b = 0; b < n && src[b] != '\0'; b++)
 		{
 			dest[a + b] = src[b];
 		}
-	}
 	dest[a + b] = '\0';
 	return (dest);
 }
