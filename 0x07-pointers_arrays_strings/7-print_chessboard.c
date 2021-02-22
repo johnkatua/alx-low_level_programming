f5d2e5e61cdb7402346b7c92104e78ef71f17e76
johnkatua <johnkatua99@gmail.com>
2021-02-22 19:56:34 +0000

    added a c program to print a chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer to pieces to print
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int a, b;
+
+	for (a = 0; a < 8; a++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[a][b]);
+		}
+		_putchar('\n');
+	}
+}
