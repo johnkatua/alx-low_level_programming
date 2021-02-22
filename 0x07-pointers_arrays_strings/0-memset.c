0ade31967e993c7d2fc05b18b3f8d2fb5e78a880
johnkatua <johnkatua99@gmail.com>
2021-02-22 12:24:14 +0000

    added holberton file and  a readme

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,10 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	char *p = (char *)s;
 
 	for (i = 0; i < n; i++)
 	{
-		p[i] = b;
+		s[i] = b;
 	}
-	return (p);
+	return (s);
 }
