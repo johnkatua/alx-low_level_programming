df02d4dd795aa71270781cb598d89cc6d900ac2f
johnkatua <johnkatua99@gmail.com>
2021-02-24 09:55:13 +0000

    added a c program to print out a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,17 @@
 #include "holberton.h"
 /**
- * _puts_recursion - It takes one argument
- * It prints a string followed by a new line
- * @s: String to be printed
- * Return: returns void
+ *_puts_recursion - prints a string followed by a new line.
+ *@s: pointer to string.
+ *
+ *Return: void.
  */
 void _puts_recursion(char *s)
 {
-	if (s == '\0')
-	{
-		_putchar('\n');
+	if (*s == '\0') /* base case */
 		return;
-	}
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(++s);
-	}
+	_putchar(*s);
+	_puts_recursion(++s);
+
+	if (*s == '\0')
+		_putchar(10);
 }
