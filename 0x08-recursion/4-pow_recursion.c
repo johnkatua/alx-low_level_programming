a7f44646a75c72345db5852db1fe4e21103c29bb
johnkatua <johnkatua99@gmail.com>
2021-02-24 11:48:06 +0000

    added a c program to print out x raised to power y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
+
 /**
- * _pow_recursion - It takes 2 arguments
- * It returns value of x raised to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y
  * @x: base number
  * @y: exponent
- * Return: returns x raised to  power y
+ *
+ * Return: x ^ y or -1 if y < 0
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	if (y == 1)
+	if (y == 0)
 		return (1);
 	return (x * _pow_recursion(x, y - 1));
 }
