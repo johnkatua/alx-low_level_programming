0c6d88ed504e48e28aa410d06a3ab83818c765f6
johnkatua <johnkatua99@gmail.com>
2021-03-02 16:03:27 +0000

    added a c program that frees a two dimensional array

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,19 +1,20 @@
 #include "holberton.h"
 #include <stdlib.h>
+
 /**
- * free_grid - It takes two arguments
- * It frees a 2 dimensional grid previously created by your alloc_grid function
+ * free_grid - frees a 2D grid previously created by alloc_grid function.
  * @grid: grid to free
  * @height: height of grid
- * Return: returns void
+ *
+ * Return: void
  */
 void free_grid(int **grid, int height)
 {
-	int a;
+	int i;
 
 	if (grid == NULL || height <= 0)
 		return;
-	for (a = 0; a < height; a++)
-		free(grid[a]);
+	for (i = 0; i < height; i++)
+		free(grid[i]);
 	free(grid);
 }
