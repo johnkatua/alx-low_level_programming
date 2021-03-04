600868532027e982dbdfac1acb7879cc184cd284
johnkatua <johnkatua99@gmail.com>
2021-03-04 16:11:09 +0000

    added a c program which allocates a memory, a readme and a header file

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,7 +5,7 @@
  * malloc_checked - It takes one argument
  * It allocates memory using malloc
  * @b: memory to allocate
- * Return: returns pointer to the allocated memory or normal process termination
+ * Return: returns pointer to allocated memory or normal process termination
  */
 void *malloc_checked(unsigned int b)
 {
@@ -13,6 +13,6 @@ void *malloc_checked(unsigned int b)
 
 	ptr = malloc(b);
 	if (ptr == NULL)
-		exit (98);
+		exit(98);
 	return (ptr);
 }
