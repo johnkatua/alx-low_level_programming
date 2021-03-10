f8bc142e21bfac8200e72018f497c66241bc85c9
johnkatua <johnkatua99@gmail.com>
2021-03-10 15:53:17 +0000

    added a function that searches for an integer in an array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_holberton.h"
+
+/**
+ * int_index - It searches for an integer
+ * @array: array with the elements
+ * @size: size of the array
+ * @cmp: pointer function to compare the elements
+ * Return: returns index of the found index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a, b;
+
+	if (size > 0 && array && cmp)
+	{
+		for (a = 0; a < size; a++)
+		{
+			b = cmp(array[i]);
+			if (b)
+				break;
+		}
+		if (a < size)
+			return (a);
+	}
+	return (-1);
+}
