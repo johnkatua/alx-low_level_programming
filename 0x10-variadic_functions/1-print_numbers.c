13216036b67130a99e5b09f1872508a3521b6690
johnkatua <johnkatua99@gmail.com>
2021-03-11 12:47:11 +0000

    added a variadic function to print number with strings in between

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - It print numbers followed by a new line
+ * @separator: string to be printed between lines
+ * @n: all numbers to be printed
+ * Return: returns void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list val;
+	int num = 0;
+	unsigned int i;
+
+	va_start(val, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(val, int);
+		printf("%d", num);
+
+		if (i < n - 1 && separator)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(val);
+}
