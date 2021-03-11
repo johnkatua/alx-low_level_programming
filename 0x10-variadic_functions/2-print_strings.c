6f479a70475bdaa1f37dc06d39e5d92c87e4c729
johnkatua <johnkatua99@gmail.com>
2021-03-11 14:14:31 +0000

    added a function that prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(string, n);
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(string, *char);
+		str = va_arg(string, char *);
 		if (str)
 			printf("%s", str);
 		else
