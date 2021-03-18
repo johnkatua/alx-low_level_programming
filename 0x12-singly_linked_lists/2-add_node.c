b4027571db2799f0b1256b23e0ec25b3eefa7b32
johnkatua <johnkatua99@gmail.com>
2021-03-18 12:19:01 +0000

    modified my add node file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,44 +1,34 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <string.h>
-
 /**
- * _strlen - finds the length of a string
- * @str: the input to find the length
- * Return: length of a string
- */
-unsigned int _strlen(char *str)
-{
-	unsigned int i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
-
-/**
- * add_node - adds a node a the beginning of a linked list
- * @head: pointer to a pointer to a linked list
- * @str: string to add to the new node
- * Return: returns pointer to the new node
+ * add_node - print a linked list
+ * @head: double pointer to structure
+ * @str: string
+ * Return: the number of nodes
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newNode;
+	list_t *new = NULL;
+	int i = 0;
 
-	if (str == NULL)
-		return (NULL);
-	newNode = malloc(sizeof(list_t));
-	if (newNode == NULL)
-		return (NULL);
-	newNode->str = strdup(str);
-	if (newNode->str == NULL)
+	if (head == '\0' || str == '\0')
+		return (0);
+	new = (list_t *)malloc(sizeof(list_t));
+	if (new == NULL)
 	{
-		free(newNode);
 		return (NULL);
 	}
-	newNode->len = _strlen(newNode->str);
-	newNode->next = *head;
-	*head = newNode;
-	return (newNode);
+	(*new).str = strdup(str);
+	if ((*new).str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	while (*(str + i))
+	{
+		i++;
+	}
+	(*new).len = i;
+	(*new).next = *head;
+	*head = new;
+	return (new);
 }
