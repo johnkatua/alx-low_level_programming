043fa06e6ba248897e75043e69a81ed07d8cdfc0
johnkatua <johnkatua99@gmail.com>
2021-03-26 07:32:51 +0000

    added a c function that prints the binary representation of number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * pow - It calculates base to power
+ * @base: base of a number
+ * @power: power of a number
+ * Return: returns base to power
+ */
+
+unsigned long int pow(unsigned int base, unsigned int power)
+{
+	unsigned long int number = 1;
+	unsigned int val;
+
+	for (val = 1; val <= power; val++)
+	{
+		number *= base;
+	}
+	return (number);
+}
+
+/**
+ * print_binary - prints a binary to represent a number
+ * @n: number to be printed
+ * Return: returns void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int a, b;
+	char j;
+
+	a = pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (a != 0)
+	{
+		b = n & a;
+		if (b == a)
+		{
+			j = 1;
+			_putchar('1');
+		}
+		else if (j == 1 || a == 1)
+		{
+			_putchar('0');
+		}
+		a >>= 1;
+	}
+}
